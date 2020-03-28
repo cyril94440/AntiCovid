f43a6b45ebf8823c3eb9fe05b616acffef4a6d98
Cyril Trosset <cyril94440@gmail.com>
2020-03-28 13:12:28 +0100

    Added HotJar

--- src/components/Page.js
+++ src/components/Page.js
@@ -64,6 +64,19 @@ export default function Page({ title, noHeader = false, children }) {
 						gtag('config', 'UA-162058754-1');`
 					}}
 				/>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1746518,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+	})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+					}}
+				/>
 				<title>{title}</title>
 			</Head>
 			<Layout
